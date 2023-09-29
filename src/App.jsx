f805feac8cd388827c4e815f08278a33aa48aab5
zubayr-parker <zubayr_parker@outlook.com>
2023-09-29 14:42:40 +1000

    added dynamic routing to api by ripping url and dynamically fetching correct api, dynamically loaded NFT Information

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/author/:authorId" element={<Author/>} />
-        <Route path="/item-details" element={<ItemDetails />} />
+        <Route path="/item-details/:nftId" element={<ItemDetails />} />
       </Routes>
       <Footer />
     </Router>
