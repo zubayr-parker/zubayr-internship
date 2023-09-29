5e406653c4fbcc70ec3a2d68256a508461c9c698
zubayr-parker <zubayr_parker@outlook.com>
2023-09-29 13:57:07 +1000

    added dynamic api fetching from url, added follow/unfollow button

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -1,85 +1,85 @@
-import React, { useEffect, useState } from "react";
-import NFTCardSkeleton from "../NFTCardSkeleton";
-import CountdownTimer from "../CountdownTimer";
-import { Link } from "react-router-dom";
+  import React, { useEffect, useState } from "react";
+  import NFTCardSkeleton from "../NFTCardSkeleton";
+  import CountdownTimer from "../CountdownTimer";
+  import { Link } from "react-router-dom";
 
-const AuthorItems = ({NFTs}, {loading}) => {
-  function renderNFTs() {
-    return loading
-      ? new Array(8).fill(0).map((_, index) => (
-          <div
-            className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-            style={{ display: "block", backgroundSize: "cover" }}
-            key={index}
-          >
-            <NFTCardSkeleton />
-          </div>
-        ))
-      : NFTs.map((NFT, index) => (
-        <div
-            className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-            style={{ display: "block", backgroundSize: "cover" }}
-            key={index}
-          >
-
-        <div className="nft__item">
-        <div className="author_list_pp">
-          <Link
-            to={`/author/${NFT.authorId}`}
-            data-bs-toggle="tooltip"
-            data-bs-placement="top"
-            title="Creator: Monica Lucas"
+  const AuthorItems = ({NFTs, loading, authorInfo}) => {
+    function renderNFTs() {
+      return loading
+        ? new Array(8).fill(0).map((_, index) => (
+            <div
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+              key={index}
             >
-            <img className="lazy" src={null} alt="" />
-            <i className="fa fa-check"></i>
-          </Link>
-        </div>
-        {NFT.expiryDate ? <CountdownTimer expiryDate={NFT.expiryDate} /> : null}
+              <NFTCardSkeleton />
+            </div>
+          ))
+        : NFTs.map((NFT, index) => (
+          <div
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+              key={index}
+            >
 
-        <div className="nft__item_wrap">
-          <div className="nft__item_extra">
-            <div className="nft__item_buttons">
-              <button>Buy Now</button>
-              <div className="nft__item_share">
-                <h4>Share</h4>
-                <a href="" target="_blank" rel="noreferrer">
-                  <i className="fa fa-facebook fa-lg"></i>
-                </a>
-                <a href="" target="_blank" rel="noreferrer">
-                  <i className="fa fa-twitter fa-lg"></i>
-                </a>
-                <a href="">
-                  <i className="fa fa-envelope fa-lg"></i>
-                </a>
+          <div className="nft__item">
+          <div className="author_list_pp">
+            <Link
+              to={`/author/${authorInfo.authorId}`}
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+              title="Creator: Monica Lucas"
+              >
+              <img className="lazy" src={authorInfo.authorImage} alt="" />
+              <i className="fa fa-check"></i>
+            </Link>
+          </div>
+          {NFT.expiryDate ? <CountdownTimer expiryDate={NFT.expiryDate} /> : null}
+
+          <div className="nft__item_wrap">
+            <div className="nft__item_extra">
+              <div className="nft__item_buttons">
+                <button>Buy Now</button>
+                <div className="nft__item_share">
+                  <h4>Share</h4>
+                  <a href="" target="_blank" rel="noreferrer">
+                    <i className="fa fa-facebook fa-lg"></i>
+                  </a>
+                  <a href="" target="_blank" rel="noreferrer">
+                    <i className="fa fa-twitter fa-lg"></i>
+                  </a>
+                  <a href="">
+                    <i className="fa fa-envelope fa-lg"></i>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
 
-          <Link to={`/item-details/${NFT.nftId}`}>
-            <img src={NFT.nftImage} className="lazy nft__item_preview" alt="" />
-          </Link>
-        </div>
-        <div className="nft__item_info">
-          <Link to={`/item-details/${NFT.nftId}`}>
-            <h4>{NFT.title}</h4>
-          </Link>
-          <div className="nft__item_price">{NFT.price} ETH</div>
-          <div className="nft__item_like">
-            <i className="fa fa-heart"></i>
-            <span>{NFT.likes}</span>
+            <Link to={`/item-details/${NFT.nftId}`}>
+              <img src={NFT.nftImage} className="lazy nft__item_preview" alt="" />
+            </Link>
           </div>
-        </div>
-      </div>    
+          <div className="nft__item_info">
+            <Link to={`/item-details/${NFT.nftId}`}>
+              <h4>{NFT.title}</h4>
+            </Link>
+            <div className="nft__item_price">{NFT.price} ETH</div>
+            <div className="nft__item_like">
+              <i className="fa fa-heart"></i>
+              <span>{NFT.likes}</span>
             </div>
-        ));
-  }
-  return (
-    <div className="de_tab_content">
-      <div className="tab-1">
-        <div className="row">{renderNFTs()}</div>
+          </div>
+        </div>    
+              </div>
+          ));
+    }
+    return (
+      <div className="de_tab_content">
+        <div className="tab-1">
+          <div className="row">{renderNFTs()}</div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default AuthorItems;
+  export default AuthorItems;
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,92 +1,116 @@
-  import React, { useEffect, useState } from "react";
-  import AuthorBanner from "../images/author_banner.jpg";
-  import { Link } from "react-router-dom";
-  import AuthorImage from "../images/author_thumbnail.jpg";
+import React, { useEffect, useState } from "react";
+import AuthorBanner from "../images/author_banner.jpg";
+import { Link, useParams } from "react-router-dom";
 import AuthorItems from "../components/author/AuthorItems";
 import axios from "axios";
 
-  const Author = () => {
-    const [NFTs, setNFTs] = useState([]);
-    const [authorInfo, setAuthorInfo] = useState([])
-    const [loading, setLoading] = useState(true);
-  
-    async function fetchNFTs() {
-      const {data} = await axios.get(
-        `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=55757699`
-      );
-  
-      setNFTs(data.nftCollection);
-      setAuthorInfo(data)
-      console.log(authorInfo);
-      setLoading(false);
+const Author = () => {
+  const [NFTs, setNFTs] = useState([]);
+  const [authorInfo, setAuthorInfo] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { authorId } = useParams();
+
+  async function fetchNFTs() {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`
+    );
+
+    setNFTs(data.nftCollection);
+    setAuthorInfo(data);
+    console.log(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchNFTs();
+  }, []);
+
+  function increaseFollowers() {
+    if (document.getElementById("follow-btn").innerHTML === "Follow") {
+      authorInfo.followers += 1;
+      document.querySelector(".profile_follower").innerHTML =
+        authorInfo.followers + " followers";
+      document.getElementById("follow-btn").innerHTML = "Unfollow";
+    } else {
+      authorInfo.followers -= 1;
+      document.querySelector(".profile_follower").innerHTML =
+        authorInfo.followers + " followers";
+      document.getElementById("follow-btn").innerHTML = "Follow";
     }
-    
-  
-    useEffect(() => {
-      fetchNFTs();
-    }, []);
+  }
 
+  return (
+    <div id="wrapper">
+      <div className="no-bottom no-top" id="content">
+        <div id="top"></div>
 
-    return (
-      <div id="wrapper">
-        <div className="no-bottom no-top" id="content">
-          <div id="top"></div>
+        <section
+          id="profile_banner"
+          aria-label="section"
+          className="text-light"
+          data-bgimage="url(images/author_banner.jpg) top"
+          style={{ background: `url(${AuthorBanner}) top` }}
+        ></section>
 
-          <section
-            id="profile_banner"
-            aria-label="section"
-            className="text-light"
-            data-bgimage="url(images/author_banner.jpg) top"
-            style={{ background: `url(${AuthorBanner}) top` }}
-          ></section>
+        <section aria-label="section">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="d_profile de-flex">
+                  <div className="de-flex-col">
+                    <div className="profile_avatar">
+                      <img src={authorInfo.authorImage} alt="" />
 
-          <section aria-label="section">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-12">
-                  <div className="d_profile de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_avatar">
-                        <img src={AuthorImage} alt="" />
-
-                        <i className="fa fa-check"></i>
-                        <div className="profile_name">
-                          <h4>
-                            Monica Lucas
-                            <span className="profile_username">@monicaaaa</span>
-                            <span id="wallet" className="profile_wallet">
-                              UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                            </span>
-                            <button id="btn_copy" title="Copy Text">
-                              Copy
-                            </button>
-                          </h4>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="profile_follow de-flex">
-                      <div className="de-flex-col">
-                        <div className="profile_follower">573 followers</div>
-                        <Link to="#" className="btn-main">
-                          Follow
-                        </Link>
+                      <i className="fa fa-check"></i>
+                      <div className="profile_name">
+                        <h4>
+                          {authorInfo.authorName}
+                          <span className="profile_username">
+                            {authorInfo.tag}
+                          </span>
+                          <span id="wallet" className="profile_wallet">
+                            {authorInfo.address}
+                          </span>
+                          <button id="btn_copy" title="Copy Text">
+                            Copy
+                          </button>
+                        </h4>
                       </div>
                     </div>
                   </div>
-                </div>
-
-                <div className="col-md-12">
-                  <div className="de_tab tab_simple">
-                    <AuthorItems NFTs={NFTs} loading={loading} />
+                  <div className="profile_follow de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_follower">
+                        {authorInfo.followers} followers
+                      </div>
+                      <Link
+                        to="#"
+                        className="btn-main"
+                        id="follow-btn"
+                        onClick={increaseFollowers}
+                      >
+                        Follow
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </section>
-        </div>
-      </div>
-    );
-    
-  };
 
-  export default Author;
+              <div className="col-md-12">
+                <div className="de_tab tab_simple">
+                  <AuthorItems
+                    NFTs={NFTs}
+                    loading={loading}
+                    authorInfo={authorInfo}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Author;
