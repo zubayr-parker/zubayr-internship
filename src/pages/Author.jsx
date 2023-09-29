0bd24b6950cd5ed4481332a85a76a11463d3a558
zubayr-parker <zubayr_parker@outlook.com>
2023-09-29 11:26:33 +1000

    made api be called in author.jsx as opposed to authoritems.jsx, then passed the info as props to authoritems.jsx

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,69 +1,92 @@
-import React from "react";
-import AuthorBanner from "../images/author_banner.jpg";
+  import React, { useEffect, useState } from "react";
+  import AuthorBanner from "../images/author_banner.jpg";
+  import { Link } from "react-router-dom";
+  import AuthorImage from "../images/author_thumbnail.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
 
-const Author = () => {
-  return (
-    <div id="wrapper">
-      <div className="no-bottom no-top" id="content">
-        <div id="top"></div>
+  const Author = () => {
+    const [NFTs, setNFTs] = useState([]);
+    const [authorInfo, setAuthorInfo] = useState([])
+    const [loading, setLoading] = useState(true);
+  
+    async function fetchNFTs() {
+      const {data} = await axios.get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=55757699`
+      );
+  
+      setNFTs(data.nftCollection);
+      setAuthorInfo(data)
+      console.log(authorInfo);
+      setLoading(false);
+    }
+    
+  
+    useEffect(() => {
+      fetchNFTs();
+    }, []);
 
-        <section
-          id="profile_banner"
-          aria-label="section"
-          className="text-light"
-          data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}
-        ></section>
 
-        <section aria-label="section">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+    return (
+      <div id="wrapper">
+        <div className="no-bottom no-top" id="content">
+          <div id="top"></div>
 
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+          <section
+            id="profile_banner"
+            aria-label="section"
+            className="text-light"
+            data-bgimage="url(images/author_banner.jpg) top"
+            style={{ background: `url(${AuthorBanner}) top` }}
+          ></section>
+
+          <section aria-label="section">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="d_profile de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_avatar">
+                        <img src={AuthorImage} alt="" />
+
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            Monica Lucas
+                            <span className="profile_username">@monicaaaa</span>
+                            <span id="wallet" className="profile_wallet">
+                              UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            </span>
+                            <button id="btn_copy" title="Copy Text">
+                              Copy
+                            </button>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">573 followers</div>
+                        <Link to="#" className="btn-main">
+                          Follow
+                        </Link>
                       </div>
                     </div>
                   </div>
-                  <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
-                    </div>
+                </div>
+
+                <div className="col-md-12">
+                  <div className="de_tab tab_simple">
+                    <AuthorItems NFTs={NFTs} loading={loading} />
                   </div>
                 </div>
               </div>
-
-              <div className="col-md-12">
-                <div className="de_tab tab_simple">
-                  <AuthorItems />
-                </div>
-              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+    
+  };
 
-export default Author;
+  export default Author;
