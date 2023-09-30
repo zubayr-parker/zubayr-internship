bc75c6da15b2f57e8a236f28738b280c55635e25
zubayr-parker <zubayr_parker@outlook.com>
2023-09-30 13:04:08 +1000

    random message

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -3,6 +3,7 @@
   import { Link } from "react-router-dom";
 
   const AuthorItems = ({NFTs, loading, authorInfo}) => {
+   //any comment 
     function renderNFTs() {
       return loading
         ? new Array(8).fill(0).map((_, index) => (
